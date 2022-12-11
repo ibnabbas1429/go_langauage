5d4aac151027ee769227a119500d3d15031aeebf
Aminu Ismail <63140904+ibnabbas1429@users.noreply.github.com>
2022-12-11 22:32:41 +0100

    Creating my first  go packaage

---
+++ hello/hello.go
@@ -0,0 +1,13 @@
+/*package hello
+import "fmt"
+func Hello(name string) string {
+	//. Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
+}*/
+
+package main
+import "fmt"
+func main(){
+	fmt.Println("Hello, world!")
+}
